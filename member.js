b2666f3ac46717b63f9f578a2385ba6d49b18a02
VieEnRose10 <91091236+VieEnRose10@users.noreply.github.com>
2023-08-02 21:25:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    var member = document.getElementById("member").value;
+    var member = member.toLowerCase();
+    var member = member.replace(/\s/g, '');
+    var member = member.replace(/[^a-zA-Z ]/
+
